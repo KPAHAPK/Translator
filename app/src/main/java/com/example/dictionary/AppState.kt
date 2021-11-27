1657e0b2fb59174f31b9cc101d6c8d83d6808d13
Roman <Roman444.zaharov@gmail.com>
2021-11-27 21:13:39 +0300

    Добавил ресурсы

--- app/src/main/java/com/example/dictionary/AppState.kt
+++ app/src/main/java/com/example/dictionary/AppState.kt
@@ -1,7 +1,7 @@
 package com.example.dictionary
 
 sealed class AppState{
-    data class Success(val data: List<DataModel>?)
-    data class Error(val error: Throwable)
-    data class Loading(val progress: Int?)
+    data class Success(val data: List<DataModel>?) : AppState()
+    data class Error(val error: Throwable): AppState()
+    data class Loading(val progress: Int?): AppState()
 }
