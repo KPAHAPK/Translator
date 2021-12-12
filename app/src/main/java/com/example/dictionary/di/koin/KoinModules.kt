b94bdcb7efda0df4216b69e62af5a4c7f6c8206b
Roman <Roman444.zaharov@gmail.com>
2021-12-12 20:42:50 +0300

    создана БД + её инжект(коин)

--- app/src/main/java/com/example/dictionary/di/koin/KoinModules.kt
+++ app/src/main/java/com/example/dictionary/di/koin/KoinModules.kt
@@ -1,27 +1,40 @@
 package com.example.dictionary.di.koin
 
+import androidx.room.Room
+import com.example.dictionary.db.HistoryDataBase
 import com.example.dictionary.di.dagger.NAME_LOCAL
 import com.example.dictionary.di.dagger.NAME_REMOTE
+import com.example.dictionary.main.HistoryInteractor
 import com.example.dictionary.model.data.DataModel
 import com.example.dictionary.model.datasource.RetrofitImplementation
 import com.example.dictionary.model.datasource.RoomDataBaseImplementation
 import com.example.dictionary.model.repository.Repository
 import com.example.dictionary.model.repository.RepositoryImplementation
+import com.example.dictionary.model.repository.RepositoryLocal
+import com.example.dictionary.model.repository.RepositoryLocalImplementation
+import com.example.dictionary.model.viewmodel.HistoryViewModel
 import com.example.dictionary.model.viewmodel.MainViewModel
 import com.example.dictionary.ui.main.MainInteractor
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val application = module {
-    single<Repository<List<DataModel>>>(named(NAME_REMOTE)) {
+    single { Room.databaseBuilder(get(), HistoryDataBase::class.java, "historyDB").build() }
+    single { get<HistoryDataBase>().historyDao() }
+    single<Repository<List<DataModel>>>() {
         RepositoryImplementation(RetrofitImplementation())
     }
-    single<Repository<List<DataModel>>>(named(NAME_LOCAL)) {
-        RepositoryImplementation(RoomDataBaseImplementation())
+    single<RepositoryLocal<List<DataModel>>>() {
+        RepositoryLocalImplementation(RoomDataBaseImplementation(get()))
     }
 }
 
 val mainScreen = module {
-    factory { MainInteractor(get(named(NAME_REMOTE)), get(named(NAME_LOCAL))) }
+    factory { MainInteractor(get(), get()) }
     factory { MainViewModel(get()) }
+}
+
+val historyScreen = module {
+    factory { HistoryViewModel(get()) }
+    factory { HistoryInteractor(get(), get()) }
 }
---
+++ app/src/main/java/com/example/dictionary/model/repository/RepositoryLocal.kt
@@ -0,0 +1,7 @@
+package com.example.dictionary.model.repository
+
+import com.example.dictionary.model.data.AppState
+
+interface RepositoryLocal<T>: Repository<T> {
+    suspend fun saveToDB(appState: AppState)
+}
